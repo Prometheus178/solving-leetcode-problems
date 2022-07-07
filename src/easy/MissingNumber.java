22f9cc547ac57220ee6a7089c28d8d58a8796faa
sergey <sergeipopof178@gmail.com>
2022-07-07 22:09:23 +0300

    Missing Number

---
+++ src/easy/MissingNumber.java
@@ -0,0 +1,24 @@
+package easy;
+
+/**
+ * Author: Sergey.
+ */
+public class MissingNumber {
+
+    public static void main(String[] args) {
+        System.out.println(solve(new int[]{0, 1}));
+    }
+
+    private static int solve(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int expectedSum = (nums.length * (nums.length + 1)) / 2;
+        int countValue = 0;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            countValue += nums[i];
+        }
+        return expectedSum - countValue;
+    }
+
+}
