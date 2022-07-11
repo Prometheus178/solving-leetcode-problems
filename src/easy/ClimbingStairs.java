bdd27db301700bcfe0e8f452f946bf34c56fc4ac
sergey <sergeipopof178@gmail.com>
2022-07-11 09:24:08 +0300

    Climbing Stairs

---
+++ src/easy/ClimbingStairs.java
@@ -0,0 +1,22 @@
+package easy;
+
+/**
+ * Author: Sergey.
+ */
+public class ClimbingStairs {
+
+    public static void main(String[] args) {
+        System.out.println(solve(5));
+    }
+
+    public static int solve(int n) {
+        int cur = 1;
+        int previous = 1;
+        for (int i = 1; i <= n; i++) {
+            int temp = cur;
+            cur = cur + previous;
+            previous = temp;
+        }
+        return previous;
+    }
+}
