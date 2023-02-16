55223ce480cbe95a4a01e4e4a4592199983dcb61
sergey <sergeipopof178@gmail.com>
2023-02-16 20:42:16 +0100

    FindDisappearedNumbers

--- src/easy/FindDisappearedNumbers.java
+++ src/easy/FindDisappearedNumbers.java
@@ -1,11 +1,11 @@
 package easy;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * 1. can solve by sort and if it has on index not equal i+1 that mean is missing num, but that to slow
- * 2. i think as we know on index we can find value of another index - 1 that be better solution
- * but what we can do if there is will be a loop.  i and value-1 is equal. ???
+ * time complexity O(n)
+ * space complexity - (if exclude result list )
  * Author: Sergey.
  */
 public class FindDisappearedNumbers {
@@ -16,28 +16,27 @@ public class FindDisappearedNumbers {
     }
 
     public static List<Integer> solve(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != i + 1) {
-//                while (nums[i] != i+1) {
-                int value = nums[i];
-                int store = nums[value - 1];
-                if (value == store) {
-
-                }
-                nums[i] = store;
-                nums[value - 1] = value;
-//                }
+        int i = 0;
+        while (i < nums.length) {
+            int correctPos = nums[i] - 1 ;
+            if (i != correctPos && nums[correctPos] != nums[i]) {
+                swap(nums, i, correctPos);
+            } else {
+                i++;
             }
         }
-
-        List<Integer> missedNums = new ArrayList();
-        for (
-                int j = 0;
-                j < nums.length; j++) {
-            if (nums[j] != j + 1) {
-                missedNums.add(j + 1);
+        List<Integer> result = new ArrayList<>();
+        for (int j = 0; j < nums.length; j++) {
+            if (j + 1 != nums[j]) {
+                result.add(j + 1);
             }
         }
-        return missedNums;
+        return result;
+    }
+
+    private static void swap(int[] nums, int i, int correctPos) {
+        nums[i] = nums[i] + nums[correctPos];
+        nums[correctPos] = nums[i] - nums[correctPos];
+        nums[i] = nums[i] - nums[correctPos];
     }
 }
