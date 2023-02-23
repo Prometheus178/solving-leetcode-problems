63176f284265e8dbf73ac0834f0301c58d5c33d6
sergey <sergeipopof178@gmail.com>
2023-02-23 17:59:01 +0100

    MergeIntervals

---
+++ src/medium/MergeIntervals.java
@@ -0,0 +1,37 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Time complexity O(n)
+ * Space complexity O(n)
+ * Author: Sergey.
+ */
+public class MergeIntervals {
+
+    public static void main(String[] args) {
+      int[][] res =  solve(new int[][]{{1,3},{2,6},{8,10},{15,18}});
+        Arrays.stream(res)
+                .forEach(a -> System.out.println(Arrays.toString(a)));
+    }
+
+    private static int[][] solve(int[][] intervals) {
+        List<int[]> merged = new ArrayList<>();
+
+        for (int i = 0; i < intervals.length; i++) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+
+            if (i > 0 && start <= merged.get(merged.size() - 1)[1]) {
+                int[] prevInterval = merged.get(merged.size() - 1);
+                prevInterval[1] = Math.max(prevInterval[1], end);
+            } else {
+                merged.add(new int[]{start, end});
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
+
